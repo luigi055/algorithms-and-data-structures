ebe7ef0f965c5be57026a61dbcddfd0570fafe7a
Pedro La Rosa D <pedrolarosad@gmail.com>
2022-08-11 16:29:06 +0200

    removing old var variable definition by using let

--- src/problem-solving/sorting-algorithm/insertion-sort/index.js
+++ src/problem-solving/sorting-algorithm/insertion-sort/index.js
@@ -1,13 +1,14 @@
 function insertionSort(array) {
-  for (let i = 1; i < array.length; i++) {
-    let currentElement = array[i];
-    for (var j = i - 1; j >= 0 && array[j] > currentElement; j--) {
-      array[j+1] = array[j];
-    }
+	for (let i = 1; i < array.length; i++) {
+		let currentElement = array[i];
+		let j;
+		for (j = i - 1; j >= 0 && array[j] > currentElement; j--) {
+			array[j + 1] = array[j];
+		}
 
-    array[j+1] = currentElement;
-  }
-  return array;
+		array[j + 1] = currentElement;
+	}
+	return array;
 }
 
 export default insertionSort;
--- src/problem-solving/sorting-algorithm/quick-sort/index.js
+++ src/problem-solving/sorting-algorithm/quick-sort/index.js
@@ -1,55 +1,50 @@
 function quickSortImmutable(array) {
-  if (array.length === 0) return []
+	if (array.length === 0) return [];
 
-  const [head, ...tail] = array;
-  const smallerElements = tail.filter(element => element <= head);
-  const biggerElements = tail.filter(element => element > head);
+	const [head, ...tail] = array;
+	const smallerElements = tail.filter((element) => element <= head);
+	const biggerElements = tail.filter((element) => element > head);
 
-  return [...quickSort(smallerElements), head, ...quickSort(biggerElements)];
+	return [...quickSort(smallerElements), head, ...quickSort(biggerElements)];
 }
 
 function swap(array, positionOne, positionTwo) {
-  if (positionOne === undefined || positionTwo === undefined) return array;
-  if (positionOne >= array.length || positionTwo >= array.length) return array
-  
-  const temporal = array[positionOne]
+	if (positionOne === undefined || positionTwo === undefined) return array;
+	if (positionOne >= array.length || positionTwo >= array.length) return array;
 
-  array[positionOne] = array[positionTwo];
-  array[positionTwo] = temporal;
+	const temporal = array[positionOne];
 
-  return array;
+	array[positionOne] = array[positionTwo];
+	array[positionTwo] = temporal;
+
+	return array;
 }
-
 
 function pivot(array, start = 0, end = array.length - 1) {
-  // We are assuming the pivot is always the first element
-  let pivot = array[start];
-  let swapIndex = start;
+	// We are assuming the pivot is always the first element
+	let pivotElement = array[start];
+	let swapIndex = start;
 
-  for (let i = start + 1; i <= end; i++) {
-    if (pivot > array[i]) {
-      swapIndex++;
-      swap(array, swapIndex, i);
-    }
-  }
+	for (let i = start + 1; i <= end; i++) {
+		if (pivotElement > array[i]) {
+			swapIndex++;
+			swap(array, swapIndex, i);
+		}
+	}
 
-  // Swap the pivot from the start the swapPoint
-  swap(array, start, swapIndex);
-  return swapIndex;
+	// Swap the pivotElement from the start the swapPoint
+	swap(array, start, swapIndex);
+	return swapIndex;
 }
 
+function quickSortMutable(array, left = 0, right = array.length - 1) {
+	if (left < right) {
+		let pivotIndex = pivot(array, left, right);
+		quickSortMutable(array, left, pivotIndex - 1);
+		quickSortMutable(array, pivotIndex + 1, right);
+	}
 
-function quickSortMutable(array, left = 0, right = array.length -1){
-  if(left < right){
-    let pivotIndex = pivot(array, left, right) //3
-    //left
-    quickSortMutable(array,left,pivotIndex-1);
-    //right
-    quickSortMutable(array,pivotIndex+1,right);
-  }
-
-   return array;
+	return array;
 }
-
 
 export default quickSortMutable;
